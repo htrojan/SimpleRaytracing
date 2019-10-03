63827084942941e5634ac701caf6d725edf4bc00
Hendrik Trojan <h.trojan.github@posteo.de>
2019-10-04 00:48:02 +0200

    Fixing handling of incoming and outgoing rays in the snellius algorithm. Outgoing rays are now recognized and calculated with a flipped normal vector and inverse refraction index.

--- src/util.rs
+++ src/util.rs
@@ -3,7 +3,7 @@ use crate::Vec3f;
 /// Returns the new direction the light ray would travel to
 /// both vectors have to be normalize to work.
 /// Returned vector is normalized.
-pub fn snellius(in_direction: &Vec3f, surface_normal: &Vec3f, refractive_index: f32) -> Vec3f {
+pub fn snellius(in_direction: &Vec3f, surface_normal: &Vec3f, refractive_index: f32) -> Option<Vec3f> {
 
     // Explanation of algorithm:
     // n1, s1, in_direction are used to construct a rectangular triangle
@@ -12,23 +12,26 @@ pub fn snellius(in_direction: &Vec3f, surface_normal: &Vec3f, refractive_index: 
     // s1 and s2 are the adjacent sides of the triangles and therefore
     // relate to each other by snellius' law.
 
-    let n1 = surface_normal; // Just for shorthand writing
 
-    let n1_length = n1.dot(in_direction) * -1.;
+    let dot = surface_normal.dot(in_direction);
+    let mut ref_index = refractive_index;
+    if dot < 0. { ref_index = 1./ref_index }
 
-    // Connection in the triangle
-    let n1 = n1 * n1_length;
-    let s1 = n1 + in_direction;
+    let n1 = -1. * surface_normal * dot;
+    let s1 = (n1 + in_direction);
     let s1_length = s1.length();
 
-    let s2_length = (refractive_index) * s1_length;
+    let s2_length = (ref_index) * s1_length;
+
+    if s2_length >= 1. { return None;} // total reflection
+
     let n2_length = f32::sqrt(1. - s2_length * s2_length);
 
-    let n2 = surface_normal * -n2_length;
+    let n2 = surface_normal * (f32::signum(dot) *n2_length);
     let s2 = s1 * (s2_length / s1_length);
 
     let result = s2 - n2;
-    result
+    Some(result)
 }
 
 #[cfg(test)]
@@ -41,7 +44,25 @@ mod tests {
         let in_dir = Vec3f::new(1., 1., 0.).normalize();
         let norm_dir = Vec3f::new(0., -1., 0.);
 
-        let out_dir = snellius(&in_dir, &norm_dir, 1.);
+        let out_dir = snellius(&in_dir, &norm_dir, 1.).unwrap();
+        assert_eq!(Vec3f::new(1., 1., 0.).normalize(), out_dir);
+    }
+
+    #[test]
+    fn test_snellius_no_refraction_opposite_normal() {
+        let in_dir = Vec3f::new(1., 1., 0.).normalize();
+        let norm_dir = Vec3f::new(0., 1., 0.);
+
+        let out_dir = snellius(&in_dir, &norm_dir, 1.).unwrap();
+        assert_eq!(Vec3f::new(1., 1., 0.).normalize(), out_dir);
+    }
+
+//    #[test]
+    fn test_snellius() {
+        let in_dir = Vec3f::new(1., 1., 0.).normalize();
+        let norm_dir = Vec3f::new(0., -1., 0.);
+
+        let out_dir = snellius(&in_dir, &norm_dir, 2.2).unwrap();
         assert_eq!(Vec3f::new(1., 1., 0.).normalize(), out_dir);
     }
 }
